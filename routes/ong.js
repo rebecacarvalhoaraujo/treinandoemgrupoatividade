2e3e5bb53a5fd967464996c6733a0da6df981f87
Becca <112990922+rebecacarvalhoaraujo@users.noreply.github.com>
2023-04-19 17:33:22 -0300

    alterações

--- routes/ong.js
+++ routes/ong.js
@@ -7,7 +7,21 @@ ong.route('/')
     res.json({mensagem: "rota ong GET"})
 })
 .post(async (req, res) => {
-    res.json({mensagem: "rota ong POST"})
+
+    const {nome, tipo, desc, cnpj, data_criacao} = req.body;
+
+    if (!nome || !tipo || !desc || !cnpj || !data_criacao) {
+        return res.status(400).json({ mensagem: "campo obrigatório não informado" });
+    }
+    
+    try {
+        const novaOng = await Ong.create(req.body);
+        res.status(201).json({mensagem: "Ong criada com sucesso, id: $(novaOng.id)"});
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+    
 })
 .put(async (req, res) => {
     res.json({mensagem: "rota ong PUT"})
